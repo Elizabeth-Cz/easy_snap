048f56923912381eb4f70116bfa298e0812e3c84
Elizabeth Czarny <liz.cz91@gmail.com>
2023-04-26 17:26:38 +0200

    partially working app, downloading different frame sizes

--- app/public/script.js
+++ app/public/script.js
@@ -5,34 +5,68 @@ const downloadBtnMobile = document.querySelector("#download-mobile");
 const downloadBtnTeblet = document.querySelector("#download-tablet");
 const downloadBtnDesktop = document.querySelector("#download-desktop");
 
-downloadBtnMobile.addEventListener("click", async () => {
-  // Disable the submit button and show "Loading" status message
-  downloadBtnMobile.disabled = true;
-  message.innerText = "Loading...";
+const BASE_URL = "http://localhost:3000";
 
+// get screenshot
+const downloadScreenshot = async (device) => {
+  message.innerText = "Loading...";
   try {
+    // in extension url will change to chroma.tabs[0] or something like that
     const url = urlInput.value;
-    const res = await fetch(
-      `http://localhost:3000/screenshot/mobile?url=${url}`
-    );
+    const res = await fetch(`${BASE_URL}/screenshot/${device}?url=${url}`);
     const blob = await res.blob();
     const urlObject = window.URL.createObjectURL(blob);
 
     // Create a link element with the screenshot as its href and download attribute
     const link = document.createElement("a");
     link.href = urlObject;
-    link.download = `${url}-mobile.png`;
+    const fileName = url.slice(8);
+    link.download = `${fileName}-${device}.png`;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-
     // Show success message
     message.innerText = "Screenshot downloaded successfully!";
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err);
     message.innerText = "Error downloading screenshot. Please try again.";
   }
+};
+// have 3 buttons, each download different size
+downloadBtnMobile.addEventListener("click", () => downloadScreenshot("mobile"));
+downloadBtnTeblet.addEventListener("click", () => downloadScreenshot("tablet"));
+downloadBtnDesktop.addEventListener("click", () =>
+  downloadScreenshot("desktop")
+);
 
-  // Enable the submit button
-  submitBtn.disabled = false;
-});
+// downloadBtnMobile.addEventListener("click", async () => {
+//   // Disable the submit button and show "Loading" status message
+//   downloadBtnMobile.disabled = true;
+//   message.innerText = "Loading...";
+
+//   try {
+//     const url = urlInput.value;
+//     const res = await fetch(
+//       `http://localhost:3000/screenshot/mobile?url=${url}`
+//     );
+//     const blob = await res.blob();
+//     const urlObject = window.URL.createObjectURL(blob);
+
+//     // Create a link element with the screenshot as its href and download attribute
+//     const link = document.createElement("a");
+//     link.href = urlObject;
+//     link.download = `${url}-mobile.png`;
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+
+//     // Show success message
+//     message.innerText = "Screenshot downloaded successfully!";
+//   } catch (error) {
+//     console.error(error);
+//     message.innerText = "Error downloading screenshot. Please try again.";
+//   }
+
+//   // Enable the submit button
+//   submitBtn.disabled = false;
+// });
--- app/server.js
+++ app/server.js
@@ -5,48 +5,108 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-// Launch Puppeteer
-(async () => {
+const viewPorts = {
+  mobile: {
+    width: 390,
+    height: 844,
+  },
+  tablet: {
+    width: 820,
+    height: 1180,
+  },
+  desktop: {
+    width: 1440,
+    height: 1080,
+  },
+};
+
+console.log(viewPorts);
+
+const corsOptions = {
+  origin: "http://127.0.0.1:5500",
+};
+app.use(cors(corsOptions));
+
+const takeScreenshot = async (req, res) => {
+  const { device } = req.params;
+  const viewport = viewPorts[device];
+  console.log(viewport);
   const browser = await puppeteer.launch({
     headless: "new",
   });
   const page = await browser.newPage();
 
-  // Endpoint for taking a screenshot
-  app.get("/screenshot/mobile", async (req, res) => {
+  try {
     const { url } = req.query;
-    try {
-      // Set the viewport to a mobile device
-      await page.setViewport({
-        width: 375,
-        height: 667,
-        deviceScaleFactor: 2,
-        isMobile: true,
-        hasTouch: true,
-        isLandscape: false,
-      });
 
-      // Navigate to the URL and wait for the page to load
-      await page.goto(url);
+    // Set the viewport based on the device type
+    await page.setViewport({
+      width: viewport.width,
+      height: viewport.height,
+      deviceScaleFactor: device === "mobile" ? 2 : null,
+      // isMobile: device === "mobile" || device === "tablet",
+      // hasTouch: device === "mobile" || device === "tablet",
+    });
 
-      // Take a screenshot of the page and return it as a PNG image
-      const screenshot = await page.screenshot({ type: "png" });
-      res.set("Content-Type", "image/png");
-      res.send(screenshot);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error taking screenshot");
-    }
-  });
-})();
+    // Navigate to the URL
+    await page.goto(url);
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+    // Wait for the page to load completely
+    // await page.waitForLoadState("networkidle");
 
-app.use(cors());
-const corsOptions = {
-  origin: "http://127.0.0.1:5500",
+    // Take a screenshot of the page
+    const screenshot = await page.screenshot({ fullPage: true });
+
+    // Send the screenshot back as a response
+    res.set("Content-Type", "image/png");
+    res.send(screenshot);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error taking screenshot");
+  } finally {
+    // Close the browser once the screenshot is taken
+    await browser.close();
+  }
 };
 
-app.use(cors(corsOptions));
+// Endpoint for taking a screenshot
+app.get("/screenshot/:device", takeScreenshot);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Launch Puppeteer ** working do not touch
+// (async () => {
+//   const browser = await puppeteer.launch({
+//     headless: "new",
+//   });
+//   const page = await browser.newPage();
+
+//   // Endpoint for taking a screenshot
+//   app.get("/screenshot/mobile", async (req, res) => {
+//     const { url } = req.query;
+//     try {
+//       // Set the viewport to a mobile device
+//       await page.setViewport({
+//         width: 375,
+//         height: 667,
+//         deviceScaleFactor: 2,
+//         isMobile: true,
+//         hasTouch: true,
+//         isLandscape: false,
+//       });
+
+//       // Navigate to the URL and wait for the page to load
+//       await page.goto(url);
+
+//       // Take a screenshot of the page and return it as a PNG image
+//       const screenshot = await page.screenshot({ type: "png" });
+//       res.set("Content-Type", "image/png");
+//       res.send(screenshot);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Error taking screenshot");
+//     }
+//   });
+// })();
